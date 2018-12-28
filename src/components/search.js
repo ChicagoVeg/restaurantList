dd2e177f787d0fea0b85e964254ed5bd80850183
Phil <zoldello@gmail.com>
2018-12-28 17:00:21 -0600

    stubbed out search funcitonality some more like add reset and search button

--- src/components/search.js
+++ src/components/search.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import PubSub from 'pubsub-js';
 import NotificationSystem from 'react-notification-system';
+import './../styles/search.scss';
 
 export class Search extends Component { 
     constructor(props) {
         super(props);
 
         this.geolocationUnsupportedSystem = React.createRef();
+        this.searchBox = React.createRef();
 
         this.state = {
             'autoDetect': true,
@@ -15,6 +17,9 @@ export class Search extends Component {
         this.showNotification = this.showNotification.bind(this);
         this.canGeolocate = this.canGeolocate.bind(this);
         this.geolocate = this.geolocate.bind(this);
+        this.performSearch = this.performSearch.bind(this);
+        this.resetSearchBox = this.resetSearchBox.bind(this);
+        this.keyPress = this.keyPress.bind(this);
     } 
     
     showNotification(options) {
@@ -25,7 +30,6 @@ export class Search extends Component {
     canGeolocate() {
         return !!window.navigator.geolocation;
     }
-
 
     geolocate() {
         const geolocation = window.navigator.geolocation;
@@ -71,6 +75,29 @@ export class Search extends Component {
             }).bind(this));
     }
 
+    performSearch() {
+        const value = this.searchBox.value;
+        const searchText = value ?
+            value.trim() :
+            ''
+
+        if (!searchText) {
+            return;
+        }
+        console.log('begin search');
+    }
+
+    resetSearchBox() {
+       this.searchBox.value = '';
+    }
+
+    keyPress(e) {
+        const key = e.key;
+        
+        if (key === 'Enter') {
+            this.performSearch()
+        }
+    }
     componentDidMount() {
         const canGeolocate = this.canGeolocate();
 
@@ -91,7 +118,6 @@ export class Search extends Component {
                 }
             );
         }
-
     }
 
     render() {
@@ -103,21 +129,70 @@ export class Search extends Component {
                         <div className="input-group-text">
                             <input type="button" 
                                 name="auto-detect-address"
-                                onClick={() =>{
-                                    this.geolocate();
-                                }}
                                 value="Auto Detect"
                             />
                         </div>
                     </div>}
-                    <input
-                        aria-label="Main search box" 
-                        autoFocus 
-                        className="form-control"
-                        name="main-search"
-                        placeholder="Search by address"
-                        type="search"
-                        />
+                    {/* search icon and box */}
+                    <svg xmlns="http://www.w3.org/2000/svg" style={{display:'none'}}>
+                        <symbol xmlns="http://www.w3.org/2000/svg" id="sbx-icon-search-14" viewBox="0 0 40 40">
+                            <path d="M26.51 28.573c-2.803 2.34-6.412 3.748-10.35 3.748C7.236 32.32 0 25.087 0 16.16 0 7.236 7.235 0 16.16 0c8.926 0 16.16 7.235 16.16 16.16 0 4.213-1.61 8.048-4.25 10.925L40 39.015l-1.524 1.524L26.51 28.572zm-10.35 2.132c8.033 0 14.545-6.512 14.545-14.544S24.193 1.617 16.16 1.617 1.617 8.128 1.617 16.16c0 8.033 6.512 14.545 14.545 14.545z"
+                                fill-rule="evenodd" />
+                        </symbol>
+                        <symbol xmlns="http://www.w3.org/2000/svg" id="sbx-icon-clear-5" viewBox="0 0 20 20">
+                         <path d="M10 20c5.523 0 10-4.477 10-10S15.523 0 10 0 0 4.477 0 10s4.477 10 10 10zm1.35-10.123l3.567 3.568-1.225 1.226-3.57-3.568-3.567 3.57-1.226-1.227 3.568-3.568-3.57-3.57 1.227-1.224 3.568 3.568 3.57-3.567 1.224 1.225-3.568 3.57zM10 18.272c4.568 0 8.272-3.704 8.272-8.272S14.568 1.728 10 1.728 1.728 5.432 1.728 10 5.432 18.272 10 18.272z"
+                            fill-rule="evenodd" />
+                        </symbol>
+                    </svg>
+                    <div 
+                        className="searchbox sbx-custom"
+                        novalidate="novalidate" 
+                        onClick={this.performSearch}
+                        onKeyPress={this.keyPress}
+                        onsubmit="return false;" 
+                    >
+                        <div role="search" className="sbx-custom__wrapper">
+                            <input 
+                                autocomplete="off" 
+                                className="sbx-custom__input"
+                                name="search"
+                                placeholder="Add an address"
+                                ref={el => this.searchBox = el}
+                                required="required" 
+                                type="search"
+                            />
+                            <button 
+                                className="sbx-custom__submit"
+                                title="Submit your search query." 
+                                type="button" 
+                            >
+                                {/*See- https://stackoverflow.com/a/27326082/178550 */}
+                                <svg 
+                                    dangerouslySetInnerHTML={{__html:'<use xlink:href="#sbx-icon-search-14"></use>'}}
+                                    onClick={() =>{
+                                        this.geolocate();
+                                    }}
+                                    role="img" aria-label="Search" 
+                                >                                  
+                                </svg>
+                            </button>
+                            <button 
+                                className="sbx-custom__reset"
+                                onClick={this.resetSearchBox}
+                                type="button" 
+                                title="Clear the search query."
+                            >
+                                <svg role="img" aria-label="Reset"  dangerouslySetInnerHTML={{__html:'<use xlink:href="#sbx-icon-clear-5"></use>'}}>
+                                </svg>
+                            </button>    
+                        </div>
+                    </div>
+
+
+
+
+
+
                 </div>    
             </div>
         );
