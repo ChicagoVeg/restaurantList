14d648c669b2e57f5b3a75aa95fbb4a11b36ff6b
Phil <zoldello@gmail.com>
2018-12-31 19:15:34 -0600

    added map provider base

---
+++ src/components/mapProviderBase.js
@@ -0,0 +1,14 @@
+import  { Component } from 'react';
+
+export class MapProviderBase extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    restaurantSelected() {}
+    updateUserAddress() {}
+    setDirectionsOnMap() {}
+    loadFullMap() {}
+}
+
+export default MapProviderBase;
